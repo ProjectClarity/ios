b0e56d183c68b99ee4dc54f56e77983622cb8ba7
Jennifer Zhang <jjz@mit.edu>
2014-09-12 21:36:22 -0400

    Initial commit with xcodeproj

---
+++ PennAppsCalendar/PennAppsCalendar/PAXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PAXAppDelegate.h
+//  PennAppsCalendar
+//
+//  Created by Jennifer Zhang on 9/12/14.
+//  Copyright (c) 2014 PAX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PAXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PennAppsCalendar/PennAppsCalendar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PennAppsCalendar
+//
+//  Created by Jennifer Zhang on 9/12/14.
+//  Copyright (c) 2014 PAX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PAXAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PAXAppDelegate class]));
+    }
+}
