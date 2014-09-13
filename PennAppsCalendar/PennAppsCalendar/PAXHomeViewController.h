080f26e108d8494247433b295904ae966f5a9df7
Jennifer Zhang <jjz@mit.edu>
2014-09-13 00:22:53 -0400

    Implemented storyboard and view controllers

---
+++ PennAppsCalendar/PennAppsCalendar/PAXHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  PAXHomeViewController.h
+//  PennAppsCalendar
+//
+//  Created by Jennifer Zhang on 9/13/14.
+//  Copyright (c) 2014 PAX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PAXHomeViewController : UIViewController
+
+@end
---
+++ PennAppsCalendar/PennAppsCalendar/PAXMoreInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  PAXMoreInfoViewController.h
+//  PennAppsCalendar
+//
+//  Created by Jennifer Zhang on 9/13/14.
+//  Copyright (c) 2014 PAX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PAXMoreInfoViewController : UIViewController
+
+@end
